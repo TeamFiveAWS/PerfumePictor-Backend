1e598eb19da8a45755010af9b34920ee4bc6bc2d
minjikim <inkmjj@sookmyung.ac.kr>
2024-07-05 13:57:45 +0900

    chore: dynamodb config
    * aws java sdk 추가
    * awssdk bom lib version update
    * dynamoDBClient credentials 주입 방법 수정

--- src/main/java/com/perfumepictor/dev/config/DynamoDBConfig.java
+++ src/main/java/com/perfumepictor/dev/config/DynamoDBConfig.java
@@ -1,9 +1,15 @@
 package com.perfumepictor.dev.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -12,10 +18,24 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @RequiredArgsConstructor
 public class DynamoDBConfig {
 
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Primary
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey));
+    }
+
     @Bean
     public DynamoDbClient dynamoDbClient(){
         return DynamoDbClient.builder().region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build();
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                )).build();
     }
 
     @Bean
