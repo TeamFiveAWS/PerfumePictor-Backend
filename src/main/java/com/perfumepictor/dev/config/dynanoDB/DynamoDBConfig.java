f43f9c6b3d4eee08a7dcc8fba827b543317af590
minjikim <inkmjj@sookmyung.ac.kr>
2024-07-18 17:06:35 +0900

    revert chore: dynamo region update

--- src/main/java/com/perfumepictor/dev/config/dynanoDB/DynamoDBConfig.java
+++ src/main/java/com/perfumepictor/dev/config/dynanoDB/DynamoDBConfig.java
@@ -34,7 +34,7 @@ public class DynamoDBConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient(){
-        return DynamoDbClient.builder().region(Region.US_WEST_2)
+        return DynamoDbClient.builder().region(Region.AP_NORTHEAST_2)
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(accessKey, secretKey)
                 )).build();
